63a257dd15c63f2e12893988fa460a4814c96283
Gianmarco Cricchio <gianmarco.cricchio02@gmail.com>
2025-05-21 23:07:51 +0200

    Aggiunte props al componente Card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import Button from "../Button";
 
-function Card() {
+interface CardProps {
+  image: string;
+  title: string;
+  text: string;
+  button: boolean;
+}
+
+const Card: React.FC<CardProps> = ({ image, title, text, button }) => {
   return (
     <>
-      <div className={styles.Card}>
-        <div className={styles.title}></div>
-        <div className={styles.image}></div>
-      </div>
+      {button === true ? (
+        <div className={styles.Card}>
+          <div className={styles.title}></div>
+          <div className={styles.image}></div>
+          <div className={styles.text}></div>
+          <Button text="CLICCA QUI" />
+        </div>
+      ) : (
+        <div className={styles.Card}>
+          <div className={styles.title}></div>
+          <div className={styles.image}></div>
+          <div className={styles.text}></div>
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default Card;
