0a6d4a5639b239335a4972d25772656cb2cae359
Gianmarco Cricchio <gianmarco.cricchio02@gmail.com>
2025-05-21 23:10:19 +0200

    Modifiche componente Card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -3,27 +3,38 @@ import styles from "./styles.module.scss";
 import Button from "../Button";
 
 interface CardProps {
-  image: string;
   title: string;
+  image: string;
   text: string;
   button: boolean;
+  buttonText: string;
 }
 
-const Card: React.FC<CardProps> = ({ image, title, text, button }) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  image,
+  text,
+  button,
+  buttonText,
+}) => {
   return (
     <>
       {button === true ? (
         <div className={styles.Card}>
-          <div className={styles.title}></div>
-          <div className={styles.image}></div>
-          <div className={styles.text}></div>
-          <Button text="CLICCA QUI" />
+          <div className={styles.title}>{title}</div>
+          <div className={styles.image}>
+            <img src={image} alt={title} />
+          </div>
+          <div className={styles.text}>{text}</div>
+          <Button text={buttonText} />
         </div>
       ) : (
         <div className={styles.Card}>
-          <div className={styles.title}></div>
-          <div className={styles.image}></div>
-          <div className={styles.text}></div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.image}>
+            <img src={image} alt={title} />
+          </div>
+          <div className={styles.text}>{text}</div>
         </div>
       )}
     </>
