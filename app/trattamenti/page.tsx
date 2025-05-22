2c288161fc61f6bbe003c20b330801f415f93ab4
Gianmarco Cricchio <gianmarco.cricchio02@gmail.com>
2025-05-22 23:31:30 +0200

    Card styles e inserimento prima card

--- app/trattamenti/page.tsx
+++ app/trattamenti/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Card from "@/components/Card";
+import styles from "./styles.module.scss";
 
 function Trattamenti() {
-  return <>trattamenti</>;
+  return (
+    <>
+      <div className={styles.Trattamenti}>
+        <Card
+          title="RICOSTRUZIONE"
+          image="/french/1.JPG"
+          text="La ricostruzione unghie è un vero e proprio allungamento dell’unghia naturale, realizzato tramite l’utilizzo delle nail form, per ottenere una forma armoniosa e resistente."
+          button={true}
+          buttonText="CONTATTAMI"
+        />
+      </div>
+    </>
+  );
 }
 
 export default Trattamenti;
